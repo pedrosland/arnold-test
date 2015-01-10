41839bb5e89b15c950e295f76eec55853ac91f4e
Pedro Sland <pedro_sland@hotmail.com>
2015-01-10 19:38:51 +0000

    Show full size image

--- src/app/vehicle/vehicle.controller.js
+++ src/app/vehicle/vehicle.controller.js
@@ -4,7 +4,18 @@ angular.module('arnoldTest')
   .controller('VehicleCtrl', function($scope, $routeParams, atResource){
 
     var obfuscatedRef = atResource.getObfuscatedStockReference($routeParams.stockRef, $routeParams.registration);
-
     $scope.imageUrls = atResource.getImageUrls(obfuscatedRef, 350);
+
+    $scope.currentImageUrl = '';
+
+    /**
+     * Set the URL of the full size image
+     *
+     * @param url {string} Image URL
+     */
+    $scope.setCurrentImage = function(url){
+      // Assume that the full sized image is available if the thumb is available
+      $scope.currentImageUrl = url.replace(/\/350\//, '/800/');
+    };
 
   });
